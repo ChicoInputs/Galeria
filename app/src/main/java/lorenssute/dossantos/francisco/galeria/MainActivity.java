1b3e805f390d3dd542adb9c8ff7d63e212dd7353
Chicoin <137420775+ChicoInputs@users.noreply.github.com>
2024-06-21 09:09:19 -0300

    Commit

--- app/src/main/java/lorenssute/dossantos/francisco/galeria/MainActivity.java
+++ app/src/main/java/lorenssute/dossantos/francisco/galeria/MainActivity.java
@@ -1,10 +1,10 @@
 package lorenssute.dossantos.francisco.galeria;
 
 import android.os.Bundle;
-import android.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
@@ -16,14 +16,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-
-        Toolbar toolbar = findViewById(R.id.tbMain);
-        setSupportActionBar(toolbar);
-
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Toolbar toolbar = findViewById(R.id.tbMain);
+        setSupportActionBar(toolbar);
     }
 }
